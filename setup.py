fe8e8e110da8c9757060b91f3a6e1f2008711854
j <johnandersen185@gmail.com>
2019-06-17 16:53:25 -0400

    Adding package details necessary for sending

---
+++ setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3.6
+
+from setuptools import setup, find_packages
+
+setup(name="saltybettor",
+      version="0.1",
+      packages=find_packages(exclude=['*.tests', '*.tests.*',
+                                      'tests.*', 'tests']),
+      package_dir={'saltbot': 'saltbot'},
+      url='https://github.com/jandersen7/saltybettor',
+      author="John Andersen",
+      author_email="johnandersen185@gmail.com",
+      scripts=['saltbot/scripts/saltybet'],
+      description="A bot for placing bets on [SaltyBet](https://www.saltybet.com), running off of selenium and the Gmail API (email alerts).",
+      install_requires=['selenium>=3.3.3', 'beautifulsoup4>=4.7.1', 'httplib2'],
+      keywords=['saltybet', 'MUGEN'])
