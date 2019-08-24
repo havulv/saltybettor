fa2afc3e9d21052c767ad048a661a37a5de70095
Jack Andersen <jandersen@plaid.com>
2019-08-24 16:14:29 -0400

    Altering saltbot script name

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name="saltbot",
       url='https://github.com/jandersen7/saltybettor',
       author="John Andersen",
       author_email="johnandersen185@gmail.com",
-      scripts=['saltbot/scripts/saltybet'],
+      scripts=['saltbot/scripts/saltbot'],
       description="A bot for placing bets on [SaltyBet](https://www.saltybet.com), running off of selenium and the Gmail API (email alerts).",
       install_requires=['selenium>=3.3.3', 'beautifulsoup4>=4.7.1', 'requests>=2.2.1', 'psycopg2>=2.8.3'],
       keywords=['saltybet', 'MUGEN'])
