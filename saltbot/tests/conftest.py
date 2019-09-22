a99800dd9e1eadfbc4657b11c932fe71fea02ef7
Jack Andersen <jandersen@plaid.com>
2019-09-21 21:11:43 -0400

    Testing: adding a faked database and building out fixtures

--- saltbot/tests/conftest.py
+++ saltbot/tests/conftest.py
@@ -1,15 +1,15 @@
 #! /usr/bin/env python3
 
+from saltbot import database
 from urllib.parse import urlparse
 from requests import cookies
-import psycopg2
 import requests
 import pytest
 import random
 import time
 
 '''
-    Requests fixture that mocks the saltybettor interface
+    Requests fixture that mocks the saltybettor interface for saltbot
 '''
 
 
@@ -204,18 +204,19 @@ def silent_requests(
                 if 't' not in kwargs.get('params').keys():
                     return MockResponse(None, 404, {})
 
+                match_cycle_time = int(time.time() - global_start) % 20
                 if url.path.lstrip('/') == 'state.json':
                     # Match should last about 15 seconds
                     # Not reality but I don't want super long tests
-                    if 8 > time.time() - global_start:
+                    if 8 > match_cycle_time:
                         return MockResponse(state_resp_open, 200, {})
-                    elif 12 > time.time() - global_start > 8:
+                    elif 12 > match_cycle_time > 8:
                         return MockResponse(state_resp_locked, 200, {})
                     else:
                         return MockResponse(state_resp_won, 200, {})
 
                 elif url.path.lstrip('/') == 'zdata.json':
-                    if 8 > time.time() - global_start:
+                    if 8 > match_cycle_time:
                         return MockResponse(zdata_resp, 200, {})
                     else:
                         return MockResponse(zdata_expired, 200, {})
@@ -226,14 +227,291 @@ def silent_requests(
 
 
 '''
-    Database fixture that mocks database calls
+    Database fixture that mocks the calls to Betdbfor saltbot
+        -- pretty simple, but needs to be built out more if we
+           want to do extensive error checking and model the behavior
+           correctly.
+        -- we don't want an actual database because we are not testing
+           the actual database calls, and are instead testing the database
+           api calls from the application
 '''
 
 
-@pytest.fixture
+@pytest.fixture(scope='function')
 def silent_database(monkeypatch):
 
-    class MockConnection():
-        pass
+    class MockBetdb():
 
-    monkeypatch.setattr(psycopg2, 'connect', MockConnection)
+        def __init__(self):
+            self.fight_table = None
+            self.fights = []
+            self.bettors_table = None
+            self.bettors = {}
+
+        def _db_guard(attr):
+            def big_wrap(func):
+                def decorator(self, *args, **kwargs):
+                    if not getattr(self, attr):
+                        raise Exception(f'{attr} not created')
+                    return func(self, *args, **kwargs)
+                return decorator
+            return big_wrap
+
+        def create_fight_table(self):
+            self.fight_table = True
+
+        @_db_guard('fight_table')
+        def new_fight(self, ftime, first_player, second_player, money1, money2, status, winner):
+            self.fights.append([ftime, first_player, second_player,
+                                money1, money2, status, winner])
+            return ftime.total_seconds()
+
+        @_db_guard('fight_table')
+        def record_fight(self, ftime, first_player, second_player,
+                         money1=0, money2=0, status='Open', winner=0):
+            if len(self.fights) == 0:
+                self.fights.append([ftime, first_player, second_player,
+                                    money1, money2, status, winner])
+            elif self.fights[-1][0] != ftime:
+                self.fights.append([ftime, first_player, second_player,
+                                    money1, money2, status, winner])
+            else:
+                self.fights[-1] = [ftime, first_player, second_player,
+                                   money1, money2, status, winner]
+
+        @_db_guard('fight_table')
+        def get_odds(self, p1, p2):
+            for i in reversed(self.fights):
+                if i[1] == p1 and i[2] == p2:
+                    return ((i[3] / (i[3] + i[4])), (i[4] / (i[3] + i[4])))
+            return None, None
+
+        @_db_guard('fight_table')
+        def get_money(self, p1, p2):
+            for i in reversed(self.fights):
+                if i[1] == p1 and i[2] == p2:
+                    return i[3], i[4]
+            return None, None
+
+        @_db_guard('fight_table')
+        def get_winner(self, p1, p2):
+            for i in reversed(self.fights):
+                if i[1] == p1 and i[2] == p2:
+                    return i[6]
+            return None
+
+        @_db_guard('fight_table')
+        def get_all_fighters(self):
+            ret = [(i[1], i[2]) for i in self.fights]
+            if ret:
+                return set([i for j in ret for i in j])
+            return None
+
+        @_db_guard('fight_table')
+        def get_all_winners(self):
+            ret = {i[i[-1]] for i in self.fights if i[-1] != 0}
+            if ret:
+                return ret
+            return None
+
+        @_db_guard('fight_table')
+        def get_all_losers(self):
+            ret = {i[(i[-1] % 2) + 1] for i in self.fights if i[-1] != 0}
+            if ret:
+                return ret
+            return None
+
+        @_db_guard('fight_table')
+        def get_all_fights(self):
+            return self.fights
+
+        @_db_guard('fight_table')
+        def get_last_fight(self):
+            return self.fights[-1]
+
+        def create_bettor_table(self):
+            self.bettors_table = True
+
+        @_db_guard('bettors_table')
+        def create_bettor(self, bettor_name, timestamp):
+            self.bettors[bettor_name] = []
+
+        @_db_guard('bettors_table')
+        def record_bettor(self, bettor_name, ftime, bal, beton=0, wager=0, rank=0):
+            if bettor_name not in self.bettors.keys():
+                raise Exception(f"{bettor_name} not created.")
+
+            for ind, bet in enumerate(self.bettors[bettor_name]):
+                if bet[0] == ftime:
+                    self.bettors[bettor_name][ind] = [ftime, bal, beton, wager, rank]
+                    return
+            self.bettors[bettor_name].append([ftime, bal, beton, wager, rank])
+
+        @_db_guard('bettors_table')
+        def record_bettors(self, bettors, fighttime):
+            for bettor_name, info in bettors.items():
+                if bettor_name not in self.bettors.keys():
+                    raise Exception(f"{bettor_name} not created.")
+                self.create_bettor(bettor_name, fighttime)
+                self.record_bettor(
+                    bettor_name, ftime=fighttime, bal=info['balance'],
+                    beton=info['bet'], wager=info['wager'], rank=info['rank'])
+
+    # Hacky way of monkeypatching all the attributes without having to write it out
+    betdb = MockBetdb()
+    for func in [meth for meth in dir(betdb) if
+                 not meth.startswith('_') and isinstance(
+                     getattr(betdb, meth),
+                     type(getattr(betdb, 'create_bettor')))]:
+        monkeypatch.setattr(database.Betdb, func, getattr(betdb, func))
+
+
+"""
+    Expected output of parsers given the state put in above
+"""
+
+
+@pytest.fixture
+def state_resp_locked_parsed():
+    return {
+        'first': {'name': "Kris", 'total': "1,603,918"},
+        'second': {'name': "Ryu EX3", 'total': "1,863,115"},
+        'status': "locked",
+        'winner': -1,
+        'tournament status': "18 more matches until the next tournament!",
+        'time': '',
+    }
+
+
+@pytest.fixture
+def state_resp_open_parsed(state_resp_locked_parsed):
+    ret = state_resp_locked_parsed
+    ret['first']['total'] = '0'
+    ret['second']['total'] = '0'
+    ret['status'] = 'open'
+    return ret
+
+
+@pytest.fixture
+def state_resp_won_parsed(state_resp_locked_parsed):
+    ret = state_resp_locked_parsed
+    ret['status'] = "2"
+    ret['tournament status'] = "17 more matches until the next tournament!"
+    return ret
+
+
+@pytest.fixture
+def zdata_resp_parsed(state_resp_locked_parsed):
+    return state_resp_locked_parsed
+
+
+@pytest.fixture
+def bettors_parsed(state_resp_locked_parsed):
+    return (
+        {
+            'bettors': {
+                "anthraxlol": {
+                    'balance': 160596752,
+                    'bet': "1",
+                    'wager': "150700",
+                    'rank': '25',
+                    'ranking': "624134",
+                },
+                "raggedclaws": {
+                    'balance': 4541155,
+                    'bet': "2",
+                    'wager': "6358",
+                    'rank': '20',
+                    'ranking': "399392",
+                },
+                "cptodinsbutt": {
+                    'balance': 1498,
+                    'bet': "2",
+                    'wager': "25",
+                    'rank': '3',
+                    'ranking': "641032",
+                },
+                "Morphemec": {
+                    'balance': 241480,
+                    'bet': "2",
+                    'wager': "1213",
+                    'rank': "17",
+                    'ranking': "497924",
+                },
+                "GuiltyGoob": {
+                    'balance': 1948,
+                    'bet': "2",
+                    'wager': "195",
+                    'rank': '4',
+                    'ranking': "639735",
+                },
+                "jimmyfranks": {
+                    'balance': 131184,
+                    'bet': "2",
+                    'wager': "5550",
+                    'rank': '25',
+                    'ranking': "44991",
+                },
+                "HateHorror": {
+                    'balance': 51892,
+                    'bet': "2",
+                    'wager': "680",
+                    'rank': '19',
+                    'ranking': "455375",
+                },
+            },
+            'time': '',
+        }, state_resp_locked_parsed)
+
+
+@pytest.fixture
+def bettors_expired_parsed():
+    return (
+        {
+            'bettors': {
+                "Justystuff": {
+                    'balance': 575,
+                    'bet': None,
+                    'wager': None,
+                    'rank': None,
+                    'ranking': "68374",
+                },
+                "FutaClaws": {
+                    'balance': 4612953,
+                    'bet': None,
+                    'wager': None,
+                    'rank': None,
+                    'ranking': "588965",
+                },
+
+                "Jofahren": {
+                    'balance': 3565441,
+                    'bet': None,
+                    'wager': None,
+                    'rank': None,
+                    'ranking': "63785",
+                },
+                "TheRedKirby": {
+                    'balance': 14273841,
+                    'bet': None,
+                    'wager': None,
+                    'rank': None,
+                    'ranking': "29275",
+                },
+                "shetaron": {
+                    'balance': 2433827,
+                    'bet': None,
+                    'wager': None,
+                    'rank': None,
+                    'ranking': "123911",
+                },
+            },
+            'time': '',
+        }, {
+            'first': {'name': "Ringlerun", 'total': "1,676,717"},
+            'second': {'name': "Rolento f. schugerg", 'total': "1,041,053"},
+            'status': "2",
+            'winner': -1,
+            'tournament status': "16 more matches until the next tournament!",
+            'time': '',
+        })
--- saltbot/tests/saltbot_test.py
+++ saltbot/tests/saltbot_test.py
@@ -19,20 +19,20 @@ def test_get_home(silent_requests, site_cookies):
     assert bot.session.cookies == site_cookies
 
 
-def test_get_state(silent_requests):
+def test_get_state(silent_requests, state_resp_open):
     bot = SaltBot(dbname='testing')
     resp = bot.get(
         'https://www.saltybet.com/state.json',
         params={'t': int(time.time())})
-    assert isinstance(resp.json(), dict)
+    assert resp.json() == state_resp_open
 
 
-def test_get_zdata(silent_requests):
+def test_get_zdata(silent_requests, zdata_resp):
     bot = SaltBot(dbname='testing')
     resp = bot.get(
         'https://www.saltybet.com/zdata.json',
         params={'t': int(time.time())})
-    assert isinstance(resp.json(), dict)
+    assert resp.json() == zdata_resp
 
 
 def test_get_bad_state(silent_requests):
@@ -45,3 +45,42 @@ def test_get_bad_zdata(silent_requests):
     with pytest.raises(Exception):
         bot = SaltBot(dbname='testing')
         bot.get('https://www.saltybet.com/zdata.json')
+
+
+def test_run(silent_requests, silent_database):
+    bot = SaltBot(dbname='testing')
+    bot.run(number=1)
+    assert bot.db.get_last_fight() is not None
+
+
+def test_get_match(silent_requests, silent_database):
+    bot = SaltBot(dbname='testing')
+    bot.get_match()
+    assert bot.db.get_last_fight() is not None
+
+
+def test_parse_match(silent_requests, silent_database,
+                     state_resp_open_parsed):
+    bot = SaltBot(dbname='testing')
+    ftime = time.time()
+    state_resp_open_parsed['time'] = ftime
+    resp = bot.get(
+        'https://www.saltybet.com/zdata.json',
+        params={'t': int(ftime)})
+    fight = bot.parse_match(resp.json(), ftime)
+    assert fight == state_resp_open_parsed
+
+
+def test_get_bettors(silent_requests, silent_database):
+    bot = SaltBot(dbname='testing')
+    pass
+
+
+def test_fight_status(silent_requests, silent_database):
+    bot = SaltBot(dbname='testing')
+    pass
+
+
+def test_bettor_status(silent_requests, silent_database):
+    bot = SaltBot(dbname='testing')
+    pass
